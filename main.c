9f841fb8ee2266fc395e355dc540cdce8823bf70
Dante Noguez <dantenoguez@gmail.com>
2024-05-04 11:42:32 -0600

    i think its correct now but gradients are vanishing

--- main.c
+++ main.c
@@ -29,7 +29,7 @@ typedef struct Layer {
 
 typedef struct MLP {
     struct Layer **layers;
-    int nouts_size;
+    int n_layers;
 } MLP;
 
 bool DEBUG = false;
@@ -71,28 +71,6 @@ float randomUniform(float min, float max) {
     return random;
 }
 
-Neuron *createNeuron(int nin) {
-    Value **weights = malloc(nin * sizeof(Value*));
-    Value *bias = malloc(sizeof(Value));
-    for (int i = 0; i < nin; i++) {
-        char *label = malloc(10 * sizeof(char)); // we're assuming up to four digit number of weights
-        sprintf(label, "weight[%d]", i);
-        weights[i] = newValue(randomUniform(-0.1, 0.1), NULL, NULL, label, 0.0, true);
-        if (DEBUG == true) {
-            printf("Neuron Weight[%d] = %f\n", i, weights[i]->data);
-        }
-    }
-    bias = newValue(randomUniform(-0.1, 0.1), NULL, NULL, "bias", 0.0, true);
-    if (DEBUG == true) {
-        printf("Neuron Bias = %f\n", bias->data);
-    }
-    Neuron *neuron = malloc(sizeof(Neuron));
-    neuron->bias = bias;
-    neuron->weights = weights;
-    neuron->nin = nin;
-    return neuron;
-}
-
 Value *sum(Value *v1, Value *v2, char *label) {
     Value **previous = createValuePointerArray(v1, v2);
     return newValue(v1->data + v2->data, "+", previous, label, 0.0, true);
@@ -121,6 +99,12 @@ Value *ReLU(Value *v, char *label) {
     return newValue(relu, "relu", previous, label, 0.0, true);
 }
 
+Value *LeakyReLU(Value *v, char *label) {
+    Value **previous = createValuePointerArray(v, NULL);
+    float leaky = (v->data > 0) ? v->data : v->data * 0.01;
+    return newValue(leaky, "leaky", previous, label, 0.0, true);
+}
+
 Value *euler(Value *v, char *label) {
     Value **previous = createValuePointerArray(v, NULL);
     float e = exp(v->data);
@@ -133,6 +117,13 @@ Value *pow2(Value *v, char *label) {
     return newValue(p, "pow2", previous, label, 0.0, true);
 }
 
+void displayValue(Value *v) {
+    float previous1 = (v->previous != NULL && v->previous[0] != NULL) ? v->previous[0]->data : 0.0;
+    float previous2 = (v->previous != NULL && v->previous[1] != NULL) ? v->previous[1]->data : 0.0;
+    char *label1 = (v->previous != NULL && v->previous[0] != NULL) ? v->previous[0]->label : "n/a";
+    char *label2 = (v->previous != NULL && v->previous[1] != NULL) ? v->previous[1]->label : "n/a";
+    printf("%s Value(data=%.2f, previous=(%.2f, %s | %.2f, %s), operation=%s, grad=%.2f)\n", v->label, v->data, previous1, label1, previous2, label2, v->operation, v->grad);
+}
 
 void _backward(Value *v) {
     if (v->operation == NULL) {
@@ -143,9 +134,13 @@ void _backward(Value *v) {
         // no grad to compute
         return;
     }
+    // if (DEBUG == true) {
+    //     printf("Backwards on:\n");
+    //     displayValue(v);
+    // }
     if (strcmp(v->operation, "+") == 0) {
-        v->previous[0]->grad += (v->previous != NULL && v->previous[0] != NULL) ? 1.0 * v->grad : 0.0;
-        v->previous[1]->grad += (v->previous != NULL && v->previous[1] != NULL) ? 1.0 * v->grad : 0.0;
+        v->previous[0]->grad += 1.0 * v->grad;
+        v->previous[1]->grad += 1.0 * v->grad;
         _backward(v->previous[0]);
         _backward(v->previous[1]);
     }
@@ -165,8 +160,12 @@ void _backward(Value *v) {
         v->previous[0]->grad += v->data * (1 - v->data) * v->grad;
         _backward(v->previous[0]);
     }
+    else if (strcmp(v->operation, "leaky") == 0) {
+        v->previous[0]->grad += (v->data > 0) ? 1 * v->grad : 0.01 * v->grad;
+        _backward(v->previous[0]);
+    }
     else if (strcmp(v->operation, "relu") == 0) {
-        v->previous[0]->grad += (v->data > 0) ? 1 : 0 * v->grad;
+        v->previous[0]->grad += (v->data > 0) ? 1 * v->grad : 0 * v->grad;
         _backward(v->previous[0]);
     }
     else if (strcmp(v->operation, "exp") == 0) {
@@ -179,30 +178,37 @@ void _backward(Value *v) {
     }
 }
 
-void displayValue(Value *v) {
-    float previous1 = (v->previous != NULL && v->previous[0] != NULL) ? v->previous[0]->data : 0.0;
-    float previous2 = (v->previous != NULL && v->previous[1] != NULL) ? v->previous[1]->data : 0.0;
-    printf("%s Value(data=%.2f, previous=(%.2f, %.2f), operation=%s, grad=%.2f)\n", v->label, v->data, previous1, previous2, v->operation, v->grad);
+Neuron *createNeuron(int nin) {
+    Value **weights = malloc(nin * sizeof(Value*));
+    Value *bias = malloc(sizeof(Value));
+    for (int i = 0; i < nin; i++) {
+        char *label = malloc(10 * sizeof(char)); // we're assuming up to four digit number of weights
+        sprintf(label, "weight[%d]", i);
+        weights[i] = newValue(randomUniform(-1.0, 1.0), NULL, NULL, label, 0.0, true);
+        if (DEBUG == true) {
+            printf("Neuron Weight[%d] = %f\n", i, weights[i]->data);
+        }
+    }
+    bias = newValue(randomUniform(-1.0, 1.0), NULL, NULL, "bias", 0.0, true);
+    if (DEBUG == true) {
+        printf("Neuron Bias = %f\n", bias->data);
+    }
+    Neuron *neuron = malloc(sizeof(Neuron));
+    neuron->bias = bias;
+    neuron->weights = weights;
+    neuron->nin = nin;
+    return neuron;
 }
 
-Value *forwardNeuron(Neuron *n, Value **x) {
-    Value **prods = malloc(n->nin * sizeof(Value*));
-    for (int i = 0; i < n->nin; i++) {
+Value *forwardNeuron(Neuron *n, Value **x, int input_size) {
+    Value *sum_prods = newValue(0.0, NULL, NULL, "sum_prods", 0.0, false);
+    for (int i = 0; i < input_size; i++) {
         char *label = malloc(10 * sizeof(char));
         sprintf(label, "x*w[%d]", i);
-        if (DEBUG == true) {
-            printf("Multiplying input %s with weight %s\n", x[i]->label, n->weights[i]->label);
-        }
-        prods[i] = mul(x[i], n->weights[i], label);
+        Value *prod = mul(x[i], n->weights[i], label);
+        sum_prods = sum(sum_prods, prod, "sum_prods");
     }
-
-    Value *sum_prods = prods[0]; // start with the first product
-    for (int i = 1; i < n->nin; i++) {
-        char *label = malloc(10 * sizeof(char));
-        sprintf(label, "sum[%d]", i);
-        sum_prods = sum(sum_prods, prods[i], label); // sum up the products
-    }
-    Value *out = htan(sum_prods, "tanh");
+    Value *out = htan(sum(sum_prods, n->bias, "sum_bias"), "tanh");
     return out;
 }
 
@@ -217,44 +223,41 @@ Layer *createLayer(int nin, int nout) {
     return layer;
 }
 
-Value **forwardLayer(Layer *layer, Value **x) {
+Value **forwardLayer(Layer *layer, Value **x, int input_size) {
     Value **outputs = malloc(layer->nout * sizeof(Value*));
     for (int i = 0; i < layer->nout; i++) {
-        outputs[i] = forwardNeuron(layer->neurons[i], x);
+        outputs[i] = forwardNeuron(layer->neurons[i], x, input_size);
     }
     return outputs;
 }
 
-MLP *createMLP(int nin, int *nouts, int nouts_size) {
-    Layer **layers = malloc(nouts_size * sizeof(Layer*));
-    for (int i = 0; i < nouts_size; i++) {
+MLP *createMLP(int nin, int *nouts, int n_layers) {
+    Layer **layers = malloc(n_layers * sizeof(Layer*));
+    for (int i = 0; i < n_layers; i++) {
         int layer_nin = (i == 0) ? nin : nouts[i-1];
         layers[i] = createLayer(layer_nin, nouts[i]);
     }
     MLP *mlp = malloc(sizeof(MLP));
     mlp->layers = layers;
-    mlp->nouts_size = nouts_size;
+    mlp->n_layers = n_layers;
     return mlp;
 }
 
 Value **forwardMLP(MLP *mlp, Value **x, int input_size) {
-    Value **outputs = malloc(input_size * sizeof(Value*));
-    for (int i = 0; i < input_size; i++) {
-        Value **input = &x[i];
-        Value **layer_outputs = input;
-        for (int j = 0; j < mlp->nouts_size; j++) {
-            if (DEBUG == true) {
-            printf("Forwarding layer %d for input %d\n", j, i);
-            }
-            layer_outputs = forwardLayer(mlp->layers[j], layer_outputs);
-        }
-        outputs[i] = layer_outputs[0];
+    Value **layer_outputs = x;
+    int layer_input_size = input_size;
+    for (int i = 0; i < mlp->n_layers; i++) {
+        // if (DEBUG == true) {
+        //     printf("Forwarding layer %d\n", i);
+        // }
+        layer_outputs = forwardLayer(mlp->layers[i], layer_outputs, layer_input_size);
+        layer_input_size = mlp->layers[i]->nout;
     }
-    return outputs;
+    return layer_outputs;
 }
 
 void freeMLP(MLP *mlp) {
-    for (int i = 0; i < mlp->nouts_size; i++) {
+    for (int i = 0; i < mlp->n_layers; i++) {
         Layer *layer = mlp->layers[i];
         for (int j = 0; j < layer->nout; j++) {
             Neuron *neuron = layer->neurons[j];
@@ -273,21 +276,27 @@ void freeMLP(MLP *mlp) {
 }
 
 void stepMLP(MLP *mlp) {
-    float learning_rate = -0.01;
-    for (int i = 0; i < mlp->nouts_size; i++) {
+    float learning_rate = 0.05;
+    for (int i = 0; i < mlp->n_layers; i++) {
         Layer *layer = mlp->layers[i];
         for (int j = 0; j < layer->nout; j++) {
             Neuron *neuron = layer->neurons[j];
             for (int k = 0; k < neuron->nin; k++) {
-                neuron->weights[k]->data += learning_rate * neuron->weights[k]->grad;
+                neuron->weights[k]->data += learning_rate * -neuron->weights[k]->grad;
+                if (DEBUG == true) {
+                    printf("Gradient for layer %d, neuron %d, weight %d: %.8f\n", i, j, k, neuron->weights[k]->grad);
+                }
             }
-            neuron->bias->data += learning_rate * neuron->bias->grad;
+            neuron->bias->data += learning_rate * -neuron->bias->grad;
+            if (DEBUG == true) {
+                printf("Gradient for layer %d, neuron %d, bias: %.8f\n", i, j, neuron->bias->grad);
+            }
         }
     }
 }
 
 void zeroGrad(MLP *mlp) {
-    for (int i = 0; i < mlp->nouts_size; i++) {
+    for (int i = 0; i < mlp->n_layers; i++) {
         Layer *layer = mlp->layers[i];
         for (int j = 0; j < layer->nout; j++) {
             Neuron *neuron = layer->neurons[j];
@@ -300,59 +309,88 @@ void zeroGrad(MLP *mlp) {
 }
 
 Value *squared_error_loss(Value *prediction, Value *target) {
-    Value *error = sum(prediction, target, "error");
-    Value *squared_error = pow2(error, "squared error");
+    Value *error = sum(prediction, mul(target, newValue(-1.0, NULL, NULL, "neg_target", 0.0, false), "neg_target"), "error");
+    Value *squared_error = mul(pow2(error, "squared_error"), newValue(0.5, NULL, NULL, "half", 0.0, false), "half_squared_error");
     return squared_error;
+}
+
+Value *average_losses(Value **losses, int count) {
+    Value *sum_losses = losses[0]; // Start with the first loss
+    for (int i = 1; i < count; i++) {
+        sum_losses = sum(sum_losses, losses[i], "sum_losses");
+    }
+    Value *avg_loss = mul(sum_losses, newValue(1.0 / count, NULL, NULL, "reciprocal_count", 0.0, false), "average_loss"); // Multiply sum by reciprocal of count
+    return avg_loss;
 }
 
 int main() {
     srand((unsigned int)time(NULL));
-    // DATASET
-    Value **data = malloc(6 * sizeof(Value*));
-    data[0] = newValue(1.0, NULL, NULL, "one", 0.0, false);
-    data[1] = newValue(2.0, NULL, NULL, "two", 0.0, false);
-    data[2] = newValue(3.0, NULL, NULL, "three", 0.0, false);
-    data[3] = newValue(4.0, NULL, NULL, "four", 0.0, false);
-    data[4] = newValue(5.0, NULL, NULL, "five", 0.0, false);
-    data[5] = newValue(6.0, NULL, NULL, "six", 0.0, false);
+    // DATA
+    // 4 data points, each containing an array of 3 Values
+    int num_features = 3;
+    int num_data_points = 4;
+    Value ***data = malloc(num_data_points * sizeof(Value**));
+    for (int i = 0; i < num_data_points; i++) {
+        data[i] = malloc(num_features * sizeof(Value*));
+        for (int j = 0; j < num_features; j++) {
+            char *label = malloc(20 * sizeof(char));
+            sprintf(label, "data[%d][%d]", i, j);
+            data[i][j] = newValue(randomUniform(-5.0, 5.0), NULL, NULL, label, 0.0, false);
+            if (DEBUG == true) {
+                printf("creating data point %f with label %s\n", data[i][j]->data, label);
+            }
+        }
+    }
     // TARGETS
-    float **targets = malloc(6 * sizeof(float*));
-    for(int i = 0; i < 6; i++) {
+    float **targets = malloc(4 * sizeof(float*));
+    for(int i = 0; i < 4; i++) {
     targets[i] = malloc(sizeof(float));
     }
     *targets[0] = -1.0;
     *targets[1] = 1.0;
     *targets[2] = -1.0;
     *targets[3] = 1.0;
-    *targets[4] = -1.0;
-    *targets[5] = 1.0;
     // NUMBER OF OUTPUTS PER LAYER
     int *nouts = malloc(3 * sizeof(int));
     nouts[0] = 4;
     nouts[1] = 4;
     nouts[2] = 1;
 
-    // TRAIN THE NETWORK
-    MLP *mlp = createMLP(1, nouts, 3);
-    Value **out = malloc(6 * sizeof(Value*));
-    for (int k = 0; k < 100; k++) {
-        Value **losses = malloc(6 * sizeof(Value*));
-        out = forwardMLP(mlp, data, 6); // forward pass
-        for (int i = 0; i < 6; i++) {
-            Value *target_value = newValue(-*targets[i], NULL, NULL, "target", 0.0, false);
-            Value *loss = squared_error_loss(out[i], target_value);
-            losses[i] = loss;
-            zeroGrad(mlp); // backward pass
-            losses[i]->grad = 1.0;
-            _backward(losses[i]);
-            stepMLP(mlp); // update
-            printf("OUTPUT: ");
-            displayValue(out[i]);
-            printf("TARGET: ");
-            displayValue(target_value);
-            printf("%d LOSS: %.2f\n", k, loss->data);
+    MLP *mlp = createMLP(3, nouts, 3);
+    // TRAINING LOOP
+    int dataset_size = 4;
+    int num_epochs = 30;
+    for (int epoch = 0; epoch < num_epochs; epoch++) {
+        Value **epoch_losses = malloc(dataset_size * sizeof(Value*));
+        for (int i = 0; i < dataset_size; i++) {
+            // Forward pass
+            Value **output = forwardMLP(mlp, data[i], 1);
+
+            // Compute loss
+            Value *target = newValue(*targets[i], NULL, NULL, "target", 0.0, false);
+            Value *loss = squared_error_loss(*output, target);
+            epoch_losses[i] = loss;
+
+            // Backward pass
+            loss->grad = 1.0;
+            _backward(loss);
+
+            // Update weights
+            stepMLP(mlp);
+
+            // Zero gradients
+            zeroGrad(mlp);
         }
-        free(losses);
+        // Compute average loss for the epoch
+        Value *avg_loss = average_losses(epoch_losses, dataset_size);
+        printf("Epoch %d, Loss: %.4f\n", epoch, avg_loss->data);
+
+        // Free memory
+        for (int i = 0; i < dataset_size; i++) {
+            free(epoch_losses[i]);
+        }
+        free(epoch_losses);
+        free(avg_loss);
     }
     freeMLP(mlp);
     return 0;
